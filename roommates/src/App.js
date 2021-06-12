ec2eb44d07c6a7a92becbbf77190a74c26a5eb45
Taewoo Kim <taewookim@Taewoos-MacBook-Pro.local>
2021-06-11 23:40:20 -0400

    create dashboard

--- roommates/src/App.js
+++ roommates/src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'; 
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import Drawer from './components/drawer/Drawer';
+import Home from './components/home/Home';
+import NotFound from './components/misc/NotFound';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
+  const [darkState, setDarkState] = useState(true);
+  const palletType = darkState ? "dark" : "light";
+  const darkTheme = createMuiTheme({
+    palette: {
+      type: palletType,
+    }
+  });
+
+  const handleThemeChange = () => {
+    setDarkState(!darkState);
+  };
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+    },
+  }));
+
+
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Router>
+        <div className={classes.root}>
+          <CssBaseline />
+          <Drawer />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
